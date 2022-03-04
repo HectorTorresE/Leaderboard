4091c0071d994cbd68f805fb7295b3a3a9704ebc
Hectorjte <hectorjte@gmail.com>
2022-03-04 14:07:35 -0600

    fix linter error

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -66,6 +66,36 @@ class leaderboard {
         `;
         return;
       }
+      if (i % 2 !== 0 && i === 1) {
+        listHtml += `
+        <div class="row borderMW bg-white">
+          <div class="col-6 text-start">
+            <h5 class="m-2">${e.user}: ${e.score}</h5>
+          </div>
+          <div class="col-6 text-end">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="silver" class="bi bi-star-fill align-self-end" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+          </svg>
+        </div>
+        </div>
+        `;
+        return;
+      }
+      if (i % 2 === 0 && i === 2) {
+        listHtml += `
+        <div class="row borderM">
+          <div class="col-6 text-start">
+            <h5 class="m-2">${e.user}: ${e.score}</h5>
+          </div>
+          <div class="col-6 text-end">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#b08d57" class="bi bi-star-fill align-self-end" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+          </svg>
+        </div>
+        </div>
+        `;
+        return;
+      }
       if (i % 2 !== 0) {
         listHtml += `
         <div class="row borderMW bg-white">
